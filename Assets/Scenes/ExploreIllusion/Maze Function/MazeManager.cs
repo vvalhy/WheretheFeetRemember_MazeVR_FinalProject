09c0140ec92ebe1eb40ce62cfe39993060744d69
yiholi <yiholi.box@gmail.com>
2026-02-15 20:08:14 +0000

    Change Maze Manager Trigger Logic
    - Set only specific trigger zone can be triggered in different Maze
    (Maze 1 can only trigger the first trigger zone etc...)

--- Assets/Scenes/ExploreIllusion/Maze Function/MazeManager.cs
+++ Assets/Scenes/ExploreIllusion/Maze Function/MazeManager.cs
@@ -2,46 +2,63 @@ using UnityEngine;
 
 public class MazeManager : MonoBehaviour
 {
-    // 在 Unity 的 Inspector 面板中，把你的三個 level 模型拉進這些格子
-    public GameObject maze1;
-    public GameObject maze2;
-    public GameObject maze3;
+    // 在 Inspector 面板中拉入對應的迷宮模型
+    public GameObject maze1; // 第一關迷宮
+    public GameObject maze2; // 第二關迷宮
+    public GameObject maze3; // 第三關迷宮
 
-    // 當有物體進入「感應區方塊」時，Unity 會自動執行這個區塊
+    // 用來區分這個感應器是哪一組：1=1轉2, 2=2轉3, 3=3轉1
+    [Header("設定感應器類型 (1, 2 或 3)")]
+    public int triggerType = 1; 
+
     private void OnTriggerEnter(Collider other)
     {
-        // 檢查進入感應區的物體，標籤 (Tag) 是不是 "MainCamera"
-        // 這樣可以確保只有玩家的頭盔能觸發切換，而不是其他物體
+        // 檢查撞擊者是否帶有 MainCamera 標籤
         if (other.CompareTag("MainCamera"))
         {
-            // 邏輯：檢查哪一個迷宮目前是開啟狀態 (activeSelf)
-            // 如果 1 號開著，就換成 2 號，依此類推達成無限循環
-            if (maze1.activeSelf)
+            // 根據不同類型的感應器執行特定的切換邏輯
+            if (triggerType == 1 && maze1.activeSelf)
             {
-                SwitchTo(2); // 呼叫下方的工具，切換到第 2 個迷宮
+                // 如果是第1組感應器，且迷宮1正開啟，則換到迷宮2
+                SwitchToMaze2();
+                Debug.Log("成功從 Maze 1 切換到 Maze 2");
             }
-            else if (maze2.activeSelf)
+            else if (triggerType == 2 && maze2.activeSelf)
             {
-                SwitchTo(3); // 呼叫下方的工具，切換到第 3 個迷宮
+                // 如果是第2組感應器，且迷宮2正開啟，則換到迷宮3
+                SwitchToMaze3();
+                Debug.Log("成功從 Maze 2 切換到 Maze 3");
             }
-            else if (maze3.activeSelf)
+            else if (triggerType == 3 && maze3.activeSelf)
             {
-                SwitchTo(1); // 呼叫下方的工具，換回第 1 個迷宮
+                // 如果是第3組感應器，且迷宮3正開啟，則換到迷宮1
+                SwitchToMaze1();
+                Debug.Log("成功從 Maze 3 切換到 Maze 1");
             }
         }
     }
 
-    // 這是一個自定義的小工具，專門用來控制三個迷宮的開關
-    // targetNumber 代表我們想要顯示哪一個迷宮
-    void SwitchTo(int targetNumber)
+    // 切換到迷宮 1 的具體執行代碼
+    void SwitchToMaze1()
     {
-        // SetActive(true) 會讓物件出現，SetActive(false) 會讓物件消失
-        // 這裡是用判斷式來決定：如果號碼相符就 true，不符就 false
-        maze1.SetActive(targetNumber == 1);
-        maze2.SetActive(targetNumber == 2);
-        maze3.SetActive(targetNumber == 3);
+        maze1.SetActive(true);
+        maze2.SetActive(false);
+        maze3.SetActive(false);
+    }
 
-        // 這行會在 Unity 編輯器左下角的 Console 視窗印出字，幫你確認程式有在跑
-        Debug.Log("玩家踩到感應區！目前切換至迷宮：" + targetNumber);
+    // 切換到迷宮 2 的具體執行代碼
+    void SwitchToMaze2()
+    {
+        maze1.SetActive(false);
+        maze2.SetActive(true);
+        maze3.SetActive(false);
+    }
+
+    // 切換到迷宮 3 的具體執行代碼
+    void SwitchToMaze3()
+    {
+        maze1.SetActive(false);
+        maze2.SetActive(false);
+        maze3.SetActive(true);
     }
 }
